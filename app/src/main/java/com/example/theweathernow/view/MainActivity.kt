45fa91b08b12a85cf82550e331e5710498bc0536
ParkSangSun1 <appleasz1028102@gmail.com>
2021-08-17 01:45:41 +0900

    [UPDATE] 패키지 구조

--- app/src/main/java/com/example/theweathernow/view/MainActivity.kt
+++ app/src/main/java/com/example/theweathernow/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.theweathernow
+package com.example.theweathernow.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.theweathernow.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
